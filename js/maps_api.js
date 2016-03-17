22675e8756415719ae3346c198824c21ade31cbd
Rafael Puyana <rfpuyana@gmail.com>
2016-03-17 15:24:15 -0500

    2 markers showing up.
    Now markers being stored in array and then internally created as
    google.maps.Markers.

--- js/maps_api.js
+++ js/maps_api.js
@@ -1,7 +1,32 @@
 var map;
-function initMap() {
+// Markers is an array with the proprty of each Marker
+// lat
+var my_data = [
+    [11, -73, "Quilla", "soundcloud url"], //Palomino Feb 07
+    [12, -74, "Palomino", "soundcloud url"] //Palomino Feb 07    
+];
+
+var my_markers = []
+
+function initMap() {  
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 11.25378, lng:-73.54848},
+        center: {lat: 11.25378, lng: -73.54848},
         zoom: 4
     });
+          
+    for(array_pos in my_data){
+        
+        console.log(my_data[array_pos]);
+        my_markers[array_pos] = new google.maps.Marker({
+            name:my_data[array_pos][4],
+            map: map,
+            position: {lat:my_data[array_pos][0], lng:my_data[array_pos][1]},        
+            title: my_data[array_pos][2],
+            soundcloud: my_data[array_pos][3]
+        });
+        
+    };
+    
+
+    console.log(my_markers); 
 }

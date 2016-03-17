40d0cdd5992e21c7eccbee9bf1ea349f015db292
Rafael Puyana <rfpuyana@gmail.com>
2016-03-17 16:27:02 -0500

    markers_data array moved to a separate js file

--- js/maps_api.js
+++ js/maps_api.js
@@ -1,13 +1,11 @@
-var map;
-// Markers is an array with the proprty of each Marker
-// lat
-var my_data = [
-    [11, -73, "Quilla", "soundcloud url"], //Palomino Feb 07
-    [12, -74, "Palomino", "soundcloud url"] //Palomino Feb 07    
-];
 
+// MARKERS DATA ARE FOUND IN markers_data.js
+
+// setup
+var map;
 var my_markers = []
 
+// Google Maps functions
 function initMap() {  
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 11.25378, lng: -73.54848},
@@ -28,5 +26,28 @@ function initMap() {
     };
     
 
-    console.log(my_markers); 
+    console.log(my_markers);
+    
+
+// historic of simple implementations
+    
+/*    
+    var col_palomino_1_marker = new google.maps.Marker({
+        map: map,
+        position: {lat:my_data[array_pos][0], lng:my_data[array_pos][1]},        
+        title: my_data[array_pos][2],
+        soundcloud: my_data[array_pos][3]
+        
+    });
+    
+*/
+/*    
+    var col_palomino_1_marker = new google.maps.Marker({
+        map: map,
+        position: {lat: 11.25378, lng: -73.54848},        
+        title: 'Click to zoom',
+        soundcloud: 'url 2 soundcloud'
+        
+    });
+*/    
 }
---
+++ js/markers_data.js
@@ -0,0 +1,4 @@
+var my_data = [
+    [11, -73, "Quilla", "soundcloud url"], //Palomino Feb 07
+    [12, -74, "Palomino", "soundcloud url"] //Palomino Feb 07    
+];

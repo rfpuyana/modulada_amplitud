b045a1996214b4ff43175a34a04da0219e69d9fc
Rafael Puyana <rfpuyana@gmail.com>
2016-03-18 07:00:30 -0500

    Added closures.html for understanding how to fix the issue with infoWindows
    Check convo here
    
    http://stackoverflow.com/questions/3059044/google-maps-js-api-v3-simple-
    multiple-marker-example

--- js/markers_data.js
+++ js/markers_data.js
@@ -1,4 +1,8 @@
-var my_data = [
-    [11, -73, "Quilla", "soundcloud url"], //Palomino Feb 07
-    [12, -74, "Palomino", "soundcloud url"] //Palomino Feb 07    
+// Lat, long, name, soundcloud track number.
+var markers_data = [
+    [11, -73, "Quilla", "252443522",], //Palomino Feb 07
+    [12, -74, "B", "252450644"] //Palomino Feb 07    
 ];
+
+
+

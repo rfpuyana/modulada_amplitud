aeb5f739556b6689608c90e4d54c75429b29974d
Rafael Puyana <rfpuyana@gmail.com>
2016-06-02 16:00:28 -0500

    New working Data.

--- js/markers_data.js
+++ js/markers_data.js
@@ -1,9 +1,21 @@
 // Lat, long, name, soundcloud track number.
 var markers_data = [
-    [11, -73, "Barranquilla", "252450644",], //Palomino Feb 07
-    [12, -74, "B", "252443522"], //Palomino Feb 07    
-    [10, -72, "B", "3"] //Palomino Feb 07        
+// Colombia
+    [10.9867843,-74.788651, "Barranquilla,Colombia. Febrero 16 - 2016", "252450644",],
+    [10.986769,-74.786671, "Barranquilla,Colombia. Febrero 07 -2016", "252443522"],
+    [4.639393, -74.062868, "Bogotá, Colombia. Junio 07 - 2015 ", "267072331"],
+    [1.143870, -77.154689, "Laguna de la Cocha, Nariño, Colombia. Junio 06 2015 ", "267142220"],
+    [1.189735, -76.973433, "Colón, Putumayo. Junio 13 - 2015", "267140734"],
+    [6.144687, -75.374654, "Rio Negro, Antioquia, Colombia. Diciembre 28 - 2015 ", "267147660"],
+
+// Mexico
+    [23.446006, -109.424981, "2015 - 09 - 28 - Cabo Pulmo,Baja California, Mexico", "267148080"],
+    [19.448360, -99.154621, "2015 - 09 - 28 - Santa María la Ribera, Ciudad de México, México", "267148080"],
+
+// US
+    [29.7317796,-95.4147422, "Houston, USA", "267164188"],
+
+// ASIA
+    [6.900756, 79.851066, "Colombo, Sri Lanka", "267164188"]
+
 ];
-
-
-
